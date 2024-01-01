b9811e56511e8380064415d2a94e12ac3786f39c
RUTIK675LT <79749663+RUTIK675LT@users.noreply.github.com>
2024-01-01 06:57:07 +0000

    2nd commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+let accountID = 12347
+const accountNum = "12345"
+
+var fName = "rutik"
+
+phone = 1234567890
+console.log(phone)
+console.log(fName)
+
+console.table([fName, accountID, accountNum, phone])
+
+
+
